8e40dd87d6ea65340aaf5cd1f62b3240cd4e3c8f
Melchizedek <melchizedek.maranga@gmail.com>
2023-03-12 18:00:18 +0300

    Hero section

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 window.onload = function () {
-    Particles.init({
-      selector: ".background",
-      maxParticle: 120,
-      speed: 0.5,
-      connectParticles: true,
-      color: "#FFFFFF",
-    });
-  };
+  Particles.init({
+    selector: ".background",
+    maxParticle: 120,
+    speed: 0.5,
+    connectParticles: true,
+    color: "#FFFFFF",
+  });
+};
